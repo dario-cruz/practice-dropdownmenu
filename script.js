4db4885f786755a8261a5af8995e083d09005256
Dario Cruz <rio2012@gmail.com>
2023-03-20 09:53:21 -0400

    Add func and eventlisteners to achieve appearing and removal of dropdown on clicks.

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+// Define all of the needed elements.
 const dropDownButton = document.getElementById('drop-down-button')
 const dropDownMenu = document.getElementById('drop-down')
 const toggleArrow = document.getElementById('arrow')
+
+// Func, when triggers will add class and start the transition in appearance and position.
+const toggleDropDown = () => {
+    dropDownMenu.classList.toggle('show')
+    toggleArrow.classList.toggle('arrow')
+}
+
+// Event for when button is clicked, triggers the above func.
+dropDownButton.addEventListener('click', (e) => {
+    e.stopPropagation() // stops function of button from being passed to parent element. Stops func from running twice.
+    toggleDropDown()
+})
+
+// Close the dropdown if being showed, when clicked away from. 
+document.documentElement.addEventListener('click', () => dropDownMenu.classList.contains('show') ? toggleDropDown(): null)
